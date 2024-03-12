877a5b02560813b503f977b1991464418e3164b4
EiHero <158979744+EiHero@users.noreply.github.com>
2024-03-11 22:25:54 -0300

    Add files via upload

---
+++ todaslojas/lojas/SCHMITT/visu/p1/JavaScript.js
@@ -0,0 +1,7 @@
+function setImage(imageSrc) {
+  var img = document.getElementById("myimage");
+  img.src = imageSrc;
+  // Reinicia o zoom para a nova imagem
+  document.querySelector(".img-zoom-lens").remove(); // Remove a lente atual
+  imageZoom("myimage", "myresult"); // Inicializa o zoom para a nova imagem
+}
