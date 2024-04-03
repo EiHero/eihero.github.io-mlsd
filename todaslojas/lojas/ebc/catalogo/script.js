62c9f8fcfb0fd167b87d2d72d8218cda11abe289
EiHero <158979744+EiHero@users.noreply.github.com>
2024-04-03 02:39:49 -0300

    Update script.js

--- todaslojas/lojas/ebc/catalogo/script.js
+++ todaslojas/lojas/ebc/catalogo/script.js
@@ -1,24 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const produtos = [
-        { categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img1.jpg' },
-		{ categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img2.jpg' },
-		{ categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img3.jpg' },
-		{ categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img4.jpg' },
-		{categoria: 'Mascaras', nome: 'Máscara de cílios vivai', descricao: 'Máscara de cílios vivai  mega volume.', preco: 'R$ 10,00', imagem: 'imagens/mascara1.jpg' },
-		{categoria: 'Sabonetes', nome: 'Sabonete', descricao: 'Sabonete líquido hidrabene.', preco: 'R$ 45,00', imagem: 'imagens/sabonete1.jpg' },
-		{categoria: 'Sabonetes', nome: 'Sabonete', descricao: 'Sabonete líquido hidrabene.', preco: 'R$ 45,00', imagem: 'imagens/sabonete2.jpg' },
-		{ categoria: 'Gloss', nome: 'Gloss laqueado ruby rose linha Glass', descricao: 'Perfeitos para seus labios.', preco: 'R$ 10,00', imagem: 'imagens/gloss1.jpg' },
-		{ categoria: 'Gloss', nome: 'Gloss laqueado ruby rose linha Glass', descricao: 'Perfeitos para seus labios.', preco: 'R$ 10,00', imagem: 'imagens/gloss2.jpg' },
-		{ categoria: 'Batons', nome: 'Batom Duo Ruby Rose', descricao: 'Perfeitos para seus labios.', preco: 'R$ 24,99', imagem: 'imagens/batom1.jpg' },
-		{categoria: 'Gels', nome: 'Gel de sobrancelhas Melu', descricao: 'O melhor gel.', preco: 'R$ 18,00', imagem: 'imagens/gel1.jpg' },
-		{ categoria: 'Bases', nome: 'Base Balla', descricao: 'acabamento matte alta cobertura.', preco: 'R$ 50,00', imagem: 'imagens/base1.jpg' },
-		{ categoria: 'Esponjas', nome: 'Esponja Flat Mirror', descricao: 'Esponja Flat Mirror Linha Glass Ruby Rose.', preco: 'R$ 20,00', imagem: 'imagens/esponja1.jpg' },
-		{ categoria: 'Esponjas', nome: 'Esponja Flat Mirror', descricao: 'Esponja Flat Mirror Linha Glass Ruby Rose.', preco: 'R$ 20,00', imagem: 'imagens/esponja2.jpg' },
-	    { categoria: 'Xampus', nome: 'Xampu Anti-Resíduos', descricao: 'Limpeza profunda e refrescante.', preco: 'R$ 19,90', imagem: 'imagens/img2.jpg' },
-		{ categoria: 'Xampus', nome: 'Xampu Anti-Resíduos', descricao: 'Limpeza profunda e refrescante.', preco: 'R$ 19,90', imagem: 'imagens/img3.jpg' },
-		{ categoria: 'Xampus', nome: 'Xampu Anti-Resíduos', descricao: 'Limpeza profunda e refrescante.', preco: 'R$ 19,90', imagem: 'imagens/img4.jpg' },
-        { categoria: 'Cremes hidratantes', nome: 'Creme Nutritivo', descricao: 'Hidratação intensa para pele seca.', preco: 'R$ 29,90', imagem: 'imagens/img2.jpg' },
-        // Adicione mais produtos aqui
+        { categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img1.jpg', pagina: 'visu/p1/visup.html' },
+        { categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img2.jpg', pagina: 'visu/p2/visup.html' },
+        { categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img3.jpg', pagina: 'visu/p3/visup.html' },
+        { categoria: 'Cílios', nome: 'Cílios Pórticios Ruby Anjo 5D', descricao: 'Perfeitos para seus olhos.', preco: 'R$ 10,00', imagem: 'imagens/img4.jpg', pagina: 'visu/p4/visup.html' },
+        // Adicione mais produtos aqui, não esqueça de incluir a propriedade 'pagina' para cada um
     ];
 
     const catalogoEl = document.getElementById('catalogo');
@@ -31,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const produtoEl = document.createElement('div');
             produtoEl.classList.add('produto');
             produtoEl.innerHTML = `
-                <img src="${produto.imagem}" onclick="window.location.href='visu/p1/visup.html';">
+                <img src="${produto.imagem}" onclick="window.location.href='${produto.pagina}';">
                 <div class="nome">${produto.nome}</div>
                 <div class="descricao">${produto.descricao}</div>
                 <div class="preco">${produto.preco}</div>
@@ -49,4 +35,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     mostrarProdutos(); // Mostra todos os produtos inicialmente
 });
-
