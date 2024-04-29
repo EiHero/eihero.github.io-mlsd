c4728ac4530b10c6ba676b749b093a4f1d8596ec
EiHero <158979744+EiHero@users.noreply.github.com>
2024-04-29 02:12:58 -0300

    Add files via upload

---
+++ comidas/lojas/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('finalizarCompra').addEventListener('click', () => {
+    // Aqui você pode implementar o envio do pedido via WhatsApp
+    // Exemplo: window.open('https://api.whatsapp.com/send?phone=SEU_NUMERO&text=PEDIDO_AQUI');
+    alert('Pedido finalizado! Aguarde o contato da Lanchonete do VTC.');
+});
